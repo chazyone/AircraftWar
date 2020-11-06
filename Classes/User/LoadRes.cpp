d3b20a6aa83fd90fa5e2f39da111ba7840bd6281
chazyone <17607120848@yeah.net>
2020-11-06 09:58:47 +0800

    add AircraftWar init

---
+++ Classes/User/LoadRes.cpp
@@ -0,0 +1,13 @@
+#include "LoadRes.h"
+
+USING_NS_CC;
+
+bool LoadRes::init()
+{
+	Layer::init();
+
+	return true;
+}
+
+
+
---
+++ Classes/User/LoadRes.h
@@ -0,0 +1,17 @@
+#ifndef __LOADRES_H__
+#define __LOADRES_H__
+
+#include "cocos2d.h"
+
+class LoadRes :public cocos2d::Layer
+{
+public:
+	bool init();
+
+
+	CREATE_FUNC(LoadRes);
+
+};
+
+
+#endif
